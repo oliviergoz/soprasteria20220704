7d1229a7caff95f90d4c9853fd4424b78134c414
Thomas Bonneaud <t.bonneaud@gmail.com>
2022-07-27 20:47:21 +0200

    Modification TicTac - FONCTIONNEL ET PAS PEU FIER - Thomas Bonneaud

--- web/sites/Thomas Bonneaud/js/tictacJS.js
+++ web/sites/Thomas Bonneaud/js/tictacJS.js
@@ -1,14 +1,42 @@
-/**
- * 
- */
-let chrono = document.getElementById('timer');
-let start = document.getElementById('butt_start');
-let stop = document.getElementById('butt_stop');
-let reset = document.getElementById('butt_reset');
+let tictac = document.getElementById('tictac');
+let start = document.getElementById('start_button');
+let stop = document.getElementById('stop_button');
+let abbort = document.getElementById('abbort_button');
+let turn = false;
 let seconde = 0;
 let minute = 0;
 
-reset.onclick = function() {
-    h1.textContent = "00:00";
-    seconde = 0; minute = 0;
+stop.disabled = true;
+abbort.disabled = true;
+
+function click_abbort() {
+    tictac.innerHTML = '00:00';
+	seconde = 0; minute = 0;
+	turn = false;
+	stop.disabled = true;
+	abbort.disabled = true;
 }
+
+function click_stop() {
+	turn = false;
+	stop.disabled = true;
+	start.disabled = false;
+}
+
+function click_start() {
+	turn = true;
+	stop.disabled = false;
+	abbort.disabled = false;
+	start.disabled = true;
+}
+
+running = setInterval(function(){
+	if (turn == true){
+		seconde++;
+		if (seconde >= 60) {
+			sececonde = 0;
+			minute++;
+		}
+		tictac.innerHTML = (minute > 9 ? minute : '0' + minute) + ':' + (seconde > 9 ? seconde : '0' + seconde);
+	}
+},1000);
