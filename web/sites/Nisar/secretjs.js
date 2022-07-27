9609b6728cd647f354156b3b2f9205410e5c36a1
Nisar <benina.nisar@gmail.com>
2022-07-27 12:52:54 +0200

    correction

--- web/sites/Nisar/secretjs.js
+++ web/sites/Nisar/secretjs.js
@@ -9,17 +9,17 @@ document.getElementById('tries').innerHTML = 'Essais Restant : '+tries+''
 function guessNumber(){
 
 	tries --;
-    guess = document.getElementById('guess').value;
-    result = document.getElementById('result').value;
-    secret = document.getElementById('secret').value;
-    if(guess == random){
-    	result = document.getElementById('result').innerHTML = 'VOUS AVEZ GAGNE'
+    guess = document.getElementById('guess')
+    result = document.getElementById('result')
+    secret = document.getElementById('secret')
+    if(guess.value == random){
+    	result.innerHTML = 'VOUS AVEZ GAGNE'
     	document.getElementById('bguess').setAttribute("hidden","");
     	document.getElementById('replay').removeAttribute("hidden","");
     }
     if(tries == 0){
-    	document.getElementById('result').innerHTML = 'VOUS AVEZ PERDU'
-    	document.getElementById('secret').value = 'Le nombre secret etait : '+random+'';
+    	result.innerHTML = 'VOUS AVEZ PERDU'
+    	secret.value = 'Le nombre secret etait : '+random+'';
     	document.getElementById('bguess').setAttribute("hidden","");
     	document.getElementById('replay').removeAttribute("hidden","");
     }
