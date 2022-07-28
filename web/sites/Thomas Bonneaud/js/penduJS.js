8b7569662e599a3803180e106d90e669394dbccd
Thomas Bonneaud <t.bonneaud@gmail.com>
2022-07-28 23:01:39 +0200

    Boutons avec Lettre + Mot random + Affichage mot - Thomas Bonneaud

--- web/sites/Thomas Bonneaud/js/penduJS.js
+++ web/sites/Thomas Bonneaud/js/penduJS.js
@@ -1,12 +1,38 @@
 let alphabet =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let mots = ['oignon', 'rouge', 'laurier', 'thym', 'daube', 'olive', 'paleron'];
+let randomWord = mots[Math.random()*mots.length|0];
 
-function creationB(letter) {
+//fonctions au lancement
+function creation() {
+	creationButton();
+	hiddenWord();
+}
+
+//fonction de creation de l'alphabet
+function creationButton() {
 	for (letter of alphabet){
 		let button = document.createElement('button');
-		button.type = 'button';
 		button.id = letter;
 		button.innerHTML = letter;
+		button.onclick = function(){
+			console.log(button.innerHTML);			//console retour valeur de l'id correct
+		};
 		let div = document.getElementById('bouton');
 		div.append(button);
+	};
+}
+
+//fonction de creation du mot caché
+function hiddenWord() {
+	let decoupe = randomWord.slice(0);
+	for(letterWord of decoupe) 	{
+		let caseLetterWord = document.createElement('input');
+		caseLetterWord.setAttribute('size','1');
+		caseLetterWord.id = letterWord;
+		caseLetterWord.value = letterWord;
+		let div = document.getElementById('word');
+		caseLetterWord.readOnly = true;
+		div.append(caseLetterWord);
+		console.log(letterWord);
 	};
 }
