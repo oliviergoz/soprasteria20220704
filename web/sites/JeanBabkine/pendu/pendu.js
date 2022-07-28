343e24f01c3fed5750aac746e3bf395f9b9b6456
navyDog <jbabkine@yahoo.com>
2022-07-28 15:07:00 +0000

    jean

--- web/sites/JeanBabkine/pendu/pendu.js
+++ web/sites/JeanBabkine/pendu/pendu.js
@@ -8,11 +8,13 @@ function clavier() {
 	let lig = document.createElement('tr');
 	for (let i in ALPHABET) {
 		let col = document.createElement('td');
-		let touche = document.createElement('input');
-		touche.type="button";
-
-		touche.value=ALPHABET[i];
-		col.append(touche);
+		let b1 = document.createElement('button');
+		b1.innerHTML = ALPHABET[i];
+		b1.id = i;
+		b1.addEventListener('click', (event) => {
+		alert(event);
+		});
+		col.append(b1);
 		lig.append(col);
 	}
 	tab.append(lig);
@@ -20,6 +22,6 @@ function clavier() {
 	
 }   
    
-   
+
    
  
