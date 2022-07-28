defe0accd21f2cfe77e1fd75049a18982833eb6e
navyDog <jbabkine@yahoo.com>
2022-07-28 12:13:29 +0000

    jean

---
+++ web/sites/JeanBabkine/js/tableauJS.js
@@ -0,0 +1,31 @@
+
+/*
+
+let produit = [
+	{nom:café,prix:1$},
+	{nom:PQ,prix:2$},
+	{nom:3monts,prix:2.5$}];
+
+
+function createTableau() {
+	//document.querySelector('#div').innerHTML =document.querySelector('#div').innerHTML+ '<h1>hello world</h1><div><h2>titre h2</h2><button onclick="alert(\'click\')">bouton</button></div>';
+	let div=document.createElement('div');
+	div.innerHTML="une div creee par le js";
+	div.id='maDiv';
+	let b1=document.createElement('button');
+	b1.innerHTML='bouton';
+	b1.id='b1';
+	b1.addEventListener('click',(event)=>{
+		console.log(event);
+	});
+	div.append(b1);
+	document.querySelector('#div').append(div);
+}
+*/
+function create() {
+	//document.querySelector('#div').innerHTML =document.querySelector('#div').innerHTML+ '<h1>hello world</h1><div><h2>titre h2</h2><button onclick="alert(\'click\')">bouton</button></div>';
+	let div=document.createElement('div');
+	div.innerHTML="une div creee par le js";
+	div.id='maDiv';
+	
+}
