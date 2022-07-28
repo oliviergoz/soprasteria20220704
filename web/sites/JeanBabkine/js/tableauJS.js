8dca58fce669e5cffc7bec8c9e8283af5dd553ad
navyDog <jbabkine@yahoo.com>
2022-07-28 13:04:14 +0000

    jean

--- web/sites/JeanBabkine/js/tableauJS.js
+++ web/sites/JeanBabkine/js/tableauJS.js
@@ -1,31 +1,55 @@
 
-/*
 
-let produit = [
-	{nom:café,prix:1$},
-	{nom:PQ,prix:2$},
-	{nom:3monts,prix:2.5$}];
+let json = [
+	{'nom':'café','prix':1},
+	{'nom':'PQ','prix':2},
+	{'nom':'3monts','prix':2.5}];
+
+
+
+
+ 
 
 
 function createTableau() {
-	//document.querySelector('#div').innerHTML =document.querySelector('#div').innerHTML+ '<h1>hello world</h1><div><h2>titre h2</h2><button onclick="alert(\'click\')">bouton</button></div>';
-	let div=document.createElement('div');
-	div.innerHTML="une div creee par le js";
-	div.id='maDiv';
-	let b1=document.createElement('button');
+	
+	let tab=document.createElement('table');
+	tab.id='course';
+	let head=document.createElement('thead');
+	head.innerHTML="Mes courses";
+	tab.append(head);
+	
+	let body=document.createElement('tbody');
+	
+	
+	
+	for (let key in json) {
+		if (json.hasOwnProperty(key)) {
+		let lig = document.createElement('tr');
+		let col = document.createElement('td');
+		let col1 = document.createElement('td');
+		col.innerHTML=json[key].nom;
+		col1.innerHTML=json[key].prix;
+		lig.append(col);
+		lig.append(col1);
+		body.append(lig);
+		}
+	}
+	tab.append(body);
+	document.querySelector('#div').append(tab);
+}
+
+/*
+
+
+let =document.createElement('button');
 	b1.innerHTML='bouton';
 	b1.id='b1';
 	b1.addEventListener('click',(event)=>{
 		console.log(event);
 	});
-	div.append(b1);
-	document.querySelector('#div').append(div);
-}
+
+
+
+
 */
-function create() {
-	//document.querySelector('#div').innerHTML =document.querySelector('#div').innerHTML+ '<h1>hello world</h1><div><h2>titre h2</h2><button onclick="alert(\'click\')">bouton</button></div>';
-	let div=document.createElement('div');
-	div.innerHTML="une div creee par le js";
-	div.id='maDiv';
-	
-}
