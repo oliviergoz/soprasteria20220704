7380050ecbf1fd94cfd29c11ca4c530ee99c98bd
Thomas Bonneaud <t.bonneaud@gmail.com>
2022-07-29 10:24:22 +0200

    message de victoire et défaite - FONCTIONNEL!!! - Thomas Bonneaud

--- web/sites/Thomas Bonneaud/js/penduJS.js
+++ web/sites/Thomas Bonneaud/js/penduJS.js
@@ -1,7 +1,9 @@
 let alphabet =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let mots = ['oignon', 'rouge', 'laurier', 'thym', 'daube', 'olive', 'paleron'];
 let randomWord = mots[Math.random()*mots.length|0];
-let compare;
+let juste = 0;		//permet de savoir combien de lettre placé
+let inc = 0;		//valeur qui incremente à chaque erreur
+let pipou = 0; 		//valeur poubelle
 
 //fonctions au lancement
 function creation() {
@@ -14,11 +16,18 @@ function creationButton() {
 	for (letter of alphabet){
 		let button = document.createElement('button');
 		button.id = letter;
+		button.className = 'truc2'
 		button.innerHTML = letter;
+		button.disabled = false;
 		button.onclick = function(){
+			button.disabled = true;
 			propal = button.innerHTML;
 			recherche();
 			console.log('but+'+button.innerHTML);			//console retour valeur de l'id correct
+			if (inc == 11){
+				alert('Vous avez perdu. Le mot etait : '+randomWord);
+				neutralization();
+			}
 		};
 		let div = document.getElementById('bouton');
 		div.append(button);
@@ -31,12 +40,15 @@ function hiddenWord() {
 	for(letterWord of decoupe) 	{
 		let caseLetterWord = document.createElement('input');
 		caseLetterWord.className = 'truc';
+		caseLetterWord.readOnly = true;
 		caseLetterWord.name = letterWord;						//on récupérera le nom comme valeur et ça fera le taff
-		caseLetterWord.value = '';								//on cache à la main
+		caseLetterWord.value = '-';								//on cache à la main
 		caseLetterWord.setAttribute('size','1');
 		let div = document.getElementById('word');
 		div.append(caseLetterWord);
+		juste++;
 		console.log(caseLetterWord.name);
+		console.log(juste);
 	};
 }
 
@@ -45,7 +57,29 @@ function recherche() {
 	for (obj of objs){
 		if (obj.name == propal){
 			obj.value = propal;
+			pipou++;
+			juste--;
+			if (juste == 0) {
+				alert('Vous avez GAGNE!. Le mot etait : '+randomWord);
+				neutralization();
+			}
 		}
 	}
-	console.log('rech+'+propal);
+	if (pipou == 0){
+		mauvaiseLettre();
+	}
+	pipou = 0;
+}
+
+function mauvaiseLettre() {
+	let imgID = 'image'+inc;
+	let img = document.getElementById(imgID).hidden=false;
+	inc++;
+}
+
+function neutralization(){		//desactive tout les boutons et affiche le mot
+	let objs = document.getElementsByClassName('truc2');
+	for (obj of objs){
+		obj.disabled = true;
+	}
 }
