9ebd18576876ecb563f6807c09dea076870d681a
olivier <o.gozlan@ajc-ingenierie.fr>
2022-08-24 16:14:23 +0200

    produitservice

--- eshop/src/main/java/eshop/services/ProduitService.java
+++ eshop/src/main/java/eshop/services/ProduitService.java
@@ -1,7 +1,6 @@
 package eshop.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Service;
 import eshop.entities.Produit;
 import eshop.exceptions.ProduitException;
 import eshop.repositories.ProduitRepository;
-import eshop.repositories.CommandeRepository;
 
 @Service
 public class ProduitService {
