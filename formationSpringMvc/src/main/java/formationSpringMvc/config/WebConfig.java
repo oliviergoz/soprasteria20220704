e7258b213243059a1ed8cef8d4935db66d2e1f88
olivier <o.gozlan@ajc-ingenierie.fr>
2022-08-25 11:59:28 +0200

    crration projet mvc

---
+++ formationSpringMvc/src/main/java/formationSpringMvc/config/WebConfig.java
@@ -0,0 +1,23 @@
+package formationSpringMvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"formationSpringMvc.controller"})
+public class WebConfig {
+
+	@Bean
+	public UrlBasedViewResolver viewResolver() {
+		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setViewClass(JstlView.class);
+		return viewResolver;
+	}
+}
