ff9b335955afdb0e2140fbdc3c7e3138dbfed6b7
olivier <o.gozlan@ajc-ingenierie.fr>
2022-08-26 11:25:24 +0200

    service fournisseur

---
+++ eshop/src/main/java/eshop/exceptions/FournisseurException.java
@@ -0,0 +1,12 @@
+package eshop.exceptions;
+
+public class FournisseurException extends RuntimeException {
+	public FournisseurException() {
+
+	}
+
+	public FournisseurException(String message) {
+		super(message);
+	}
+
+}
---
+++ eshop/src/main/java/eshop/services/FournisseurService.java
@@ -0,0 +1,49 @@
+package eshop.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import eshop.entities.Fournisseur;
+import eshop.exceptions.FournisseurException;
+import eshop.repositories.FournisseurRepository;
+
+@Service
+public class FournisseurService {
+	@Autowired
+	private FournisseurRepository fournisseurRepo;
+
+	public Fournisseur create(Fournisseur fournisseur) {
+		return fournisseurRepo.save(fournisseur);
+	}
+
+	public Fournisseur getById(Long id) {
+		return fournisseurRepo.findById(id).orElseThrow(FournisseurException::new);
+	}
+
+	public Fournisseur getByIdWithProduits(Long id) {
+		return fournisseurRepo.findByIdWithProduits(id).orElseThrow(FournisseurException::new);
+	}
+
+	public Fournisseur update(Fournisseur fournisseur) {
+		Fournisseur fournisseurEnBase = getById(fournisseur.getId());
+		fournisseurEnBase.setNom(fournisseur.getNom());
+		fournisseurEnBase.setAdresse(fournisseur.getAdresse());
+		fournisseurEnBase.setContact(fournisseur.getContact());
+		return fournisseurRepo.save(fournisseurEnBase);
+	}
+
+	public List<Fournisseur> getAll() {
+		return fournisseurRepo.findAll();
+	}
+
+	public void delete(Fournisseur fournisseur) {
+		fournisseurRepo.delete(fournisseur);
+	}
+
+	public void deleteById(Long id) {
+		delete(getById(id));
+	}
+
+}
